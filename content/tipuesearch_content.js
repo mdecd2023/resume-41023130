26ed60fb2be74f88cda0f09cf848b45aaaeaec34
41023130 <car48383118@gmail.com>
2023-06-05 22:50:44 +0800

    0605-2

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\xa0 \n 這是計算機程式課程 2021 Fall 的作業倉儲與網站 template \n 41023130666 \n 777 \n 888', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'First', 'text': '', 'tags': '', 'url': 'First.html'}, {'title': 'About', 'text': '/downloads/2 (2).pdf \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
